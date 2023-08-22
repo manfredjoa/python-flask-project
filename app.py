f0eebee98259a2f2340fe53ce5ed06d8c576ec84
manfredjoa <joamanfred@gmail.com>
2023-08-22 18:17:11 -0400

    add CRUD functions tested with Postman

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from peewee import *
 from playhouse.shortcuts import model_to_dict, dict_to_model
 import json
@@ -41,7 +41,34 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'This is working!'
+    return 'This is the root route.'
+
+
+@app.route('/wines', methods=['GET', 'POST'])
+@app.route('/wines/<id>', methods=['GET', 'PUT', 'DELETE'])
+def endpoint(id=None):
+    if request.method == 'GET':
+        if id:
+            return jsonify(model_to_dict(Wine.get(Wine.id == id)))
+        else:
+            wine_list = []
+            for wine in Wine.select():
+                wine_list.append(model_to_dict(wine))
+            return jsonify(wine_list)
+
+    if request.method == 'PUT':
+        body = request.get_json()
+        Wine.update(body).where(Wine.id == id).execute()
+        return "Wine " + str(id) + " has been updated."
+
+    if request.method == 'POST':
+        new_wine = dict_to_model(Wine, request.get_json())
+        new_wine.save()
+        return f"{new_wine.name} has been added."
+
+    if request.method == 'DELETE':
+        Wine.delete().where(Wine.id == id).execute()
+        return "Wine " + str(id) + " has been deleted."
 
 
 app.run(port=5000, debug=True)
