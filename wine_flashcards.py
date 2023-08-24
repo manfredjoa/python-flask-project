7a134e33f2a75b0c98f1a1a2704351403ee58712
manfredjoa <joamanfred@gmail.com>
2023-08-24 12:14:03 -0400

    added play again function

--- wine_flashcards.py
+++ wine_flashcards.py
@@ -11,57 +11,77 @@ def wine_flashcards():
 
     number_of_wines = len(wine_list)
 
-    random.shuffle(wine_list)
-
     def train():
-        number_of_cards = int(input(
-            f"Let's train! How many cards would you like to use (1 - {number_of_wines})? "))
+        random.shuffle(wine_list)
+        correct = 0
+        incorrect = 0
+        play_again = "y"
 
-        for i in range(0, number_of_cards):
-            response = input(
-                f"What type of wine is {wine_list[i]['name']} (red, white, or rose)? ")
+        while (play_again.lower() == "y"):
+            number_of_cards = int(input(
+                f"Let's train! How many cards would you like to use (1 - {number_of_wines})? "))
 
-            if wine_list[i]['product_type'] == "Red Wine":
-                red = wine_list[i]['product_type'][0:3].lower()
-                if response.lower() == red:
-                    print("Correct!")
-                    flashcard = Wine.get(Wine.id == wine_list[i]['id'])
-                    flashcard.correct += 1
-                    flashcard.save()
+            for i in range(0, number_of_cards):
+                response = input(
+                    f"What type of wine is {wine_list[i]['name']} (red, white, or rose)? ")
+
+                if wine_list[i]['product_type'] == "Red Wine":
+                    red = wine_list[i]['product_type'][0:3].lower()
+                    if response.lower() == red:
+                        print("Correct!")
+                        flashcard = Wine.get(Wine.id == wine_list[i]['id'])
+                        flashcard.correct += 1
+                        flashcard.save()
+                        correct += 1
+                    else:
+                        print(
+                            f"Incorrect! {wine_list[i]['name']} is a {wine_list[i]['product_type']}.")
+                        flashcard = Wine.get(Wine.id == wine_list[i]['id'])
+                        flashcard.incorrect += 1
+                        flashcard.save()
+                        incorrect += 1
+
+                elif wine_list[i]['product_type'] == "White Wine":
+                    white = wine_list[i]['product_type'][0:5].lower()
+                    if response.lower() == white:
+                        print("Correct!")
+                        flashcard = Wine.get(Wine.id == wine_list[i]['id'])
+                        flashcard.correct += 1
+                        flashcard.save()
+                        correct += 1
+                    else:
+                        print(
+                            f"Incorrect! {wine_list[i]['name']} is a {wine_list[i]['product_type']}.")
+                        flashcard = Wine.get(Wine.id == wine_list[i]['id'])
+                        flashcard.incorrect += 1
+                        flashcard.save()
+                        incorrect += 1
+
                 else:
-                    print(
-                        f"Incorrect! {wine_list[i]['name']} is a {wine_list[i]['product_type']}.")
-                    flashcard = Wine.get(Wine.id == wine_list[i]['id'])
-                    flashcard.incorrect += 1
-                    flashcard.save()
+                    rose = wine_list[i]['product_type'][0:4].lower()
+                    if response.lower() == rose:
+                        print("Correct!")
+                        flashcard = Wine.get(Wine.id == wine_list[i]['id'])
+                        flashcard.correct += 1
+                        flashcard.save()
+                        correct += 1
+                    else:
+                        print(
+                            f"Incorrect! {wine_list[i]['name']} is a {wine_list[i]['product_type']}.")
+                        flashcard = Wine.get(Wine.id == wine_list[i]['id'])
+                        flashcard.incorrect += 1
+                        flashcard.save()
+                        incorrect += 1
 
-            elif wine_list[i]['product_type'] == "White Wine":
-                white = wine_list[i]['product_type'][0:5].lower()
-                if response.lower() == white:
-                    print("Correct!")
-                    flashcard = Wine.get(Wine.id == wine_list[i]['id'])
-                    flashcard.correct += 1
-                    flashcard.save()
-                else:
-                    print(
-                        f"Incorrect! {wine_list[i]['name']} is a {wine_list[i]['product_type']}.")
-                    flashcard = Wine.get(Wine.id == wine_list[i]['id'])
-                    flashcard.incorrect += 1
-                    flashcard.save()
-
+            play_again_response = input(
+                f"You got {correct} correct and {incorrect} incorrect. Would you like to play again (y or n)? ")
+            if play_again_response.lower() == "y":
+                play_again = "y"
+                correct = 0
+                incorrect = 0
             else:
-                rose = wine_list[i]['product_type'][0:4].lower()
-                if response.lower() == rose:
-                    print("Correct!")
-                    flashcard = Wine.get(Wine.id == wine_list[i]['id'])
-                    flashcard.correct += 1
-                    flashcard.save()
-                else:
-                    print(
-                        f"Incorrect! {wine_list[i]['name']} is a {wine_list[i]['product_type']}.")
-                    flashcard = Wine.get(Wine.id == wine_list[i]['id'])
-                    flashcard.incorrect += 1
-                    flashcard.save()
+                play_again = "n"
+                print("It was nice training with you today. Goodbye!")
 
     def start():
         create_or_train = input(
